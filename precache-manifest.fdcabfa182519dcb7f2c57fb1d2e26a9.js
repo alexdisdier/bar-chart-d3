d88c7ab5182d7ce11cc26179351839c97532df75
Alexandre Disdier <28678511+alexdisdier@users.noreply.github.com>
2019-01-07 14:56:13 +0100

    Updates

---
+++ precache-manifest.fdcabfa182519dcb7f2c57fb1d2e26a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f470b2fec6917a577207",
+    "url": "/bar-chart-d3/static/js/runtime~main.f470b2fe.js"
+  },
+  {
+    "revision": "60bc191afde07b04a4b7",
+    "url": "/bar-chart-d3/static/js/main.60bc191a.chunk.js"
+  },
+  {
+    "revision": "c866b1943e1c946a7719",
+    "url": "/bar-chart-d3/static/js/1.c866b194.chunk.js"
+  },
+  {
+    "revision": "60bc191afde07b04a4b7",
+    "url": "/bar-chart-d3/static/css/main.1a523caf.chunk.css"
+  },
+  {
+    "revision": "fe6943a8780f7cdf85df60360efb4905",
+    "url": "/bar-chart-d3/index.html"
+  }
+];
